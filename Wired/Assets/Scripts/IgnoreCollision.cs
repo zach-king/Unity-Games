5d3f0edd830064d3de6e1ce5a8b59899b5f80787
Zachary King <kingzach77@gmail.com>
2016-08-27 00:16:09 -0500

    Initial work with testing scene (rope and character)

---
+++ Wired/Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour {
+
+    public Collider ignoredCollider;
+    public string ignoreLayer1, ignoreLayer2;
+
+	// Use this for initialization
+	void Start () {
+        //Physics.IgnoreCollision(GetComponent<Collider>(), ignoredCollider);
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer(ignoreLayer1), LayerMask.NameToLayer(ignoreLayer2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

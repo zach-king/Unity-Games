57829fc86998028980f5683d5060b63b91a784bb
Zachary King <kingzach77@gmail.com>
2016-09-04 00:30:10 -0500

    Wire working with physics

---
+++ Wired/Assets/Scripts/RopeBreak.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeBreak : MonoBehaviour
+{
+    public UltimateRope rope;
+
+    bool broken;
+
+    void Start()
+    {
+        broken = false;
+    }
+
+    void OnRopeBreak(UltimateRope.RopeBreakEventInfo breakInfo)
+    {
+        if (breakInfo.rope == rope)
+        {
+            broken = true;
+            Debug.Log("Rope Broken!");
+            
+        }
+
+    }
+
+    void Update()
+    {
+    }
+}

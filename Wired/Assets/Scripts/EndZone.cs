030b77dc6d4c1b4cdf7faabe31414adff9c4cf8c
Zachary King <kingzach77@gmail.com>
2016-09-05 01:29:11 -0500

    Added more links in 'rope'. Still having Physics bugs

---
+++ Wired/Assets/Scripts/EndZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndZone : MonoBehaviour {
+    // Player collided with end zone?
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            // Perform end-game action...
+            Debug.Log("You won!");
+        }
+    }
+}
--- Wired/Assets/Scripts/IgnoreCollision.cs
+++ Wired/Assets/Scripts/IgnoreCollision.cs
@@ -3,17 +3,14 @@ using System.Collections;
 
 public class IgnoreCollision : MonoBehaviour {
 
-    public Collider ignoredCollider;
-    public string ignoreLayer1, ignoreLayer2;
+    public string ignoreLayer;
+
+    private UltimateRope rope;
 
 	// Use this for initialization
 	void Start () {
-        //Physics.IgnoreCollision(GetComponent<Collider>(), ignoredCollider);
-        Physics.IgnoreLayerCollision(LayerMask.NameToLayer(ignoreLayer1), LayerMask.NameToLayer(ignoreLayer2));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+        rope = GetComponent<UltimateRope>();
+
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer(ignoreLayer), LayerMask.NameToLayer(LayerMask.LayerToName(rope.RopeLayer)));
 	}
 }
